e47b8e78cf9d69a5ee3f9f769d4d90a58f1339d1
Anandsoni992 <anandsoni9165@gmail.com>
2025-03-05 13:51:00 +0530

    Anand add . adding bridgelabz logo

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string = "Hello from BridgeLabz";  // Define message property
+  message: string = "Hello from BridgeLabz";  
+  logoPath: string = "E:/Capg/Angular/my-angular-app/public/BL_logo_square_jpg.jpg";  
 }
